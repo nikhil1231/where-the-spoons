a02fc33387d54994534b871c373c2e81286eeda6
nikhil1231 <nikhil1231@hotmail.co.uk>
2019-09-22 22:19:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# WhereTheSpoons
+
+### Introduction
+Can't decide which 'spoons to head to with your mates? Use WhereTheSpoons to find one bang in the middle of everyone!
+Just create a new sesh, send the link to your friends, and put your addresses in - it's that simple.
---
+++ js/index.js
@@ -0,0 +1,34 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyB2kTxjCv5qeItZPKkAV84nSI6CR_-9SXU",
+    authDomain: "wherethespoons.firebaseapp.com",
+    databaseURL: "https://wherethespoons.firebaseio.com",
+    projectId: "wherethespoons",
+    storageBucket: "",
+    messagingSenderId: "47429380907",
+    appId: "1:47429380907:web:7b5171603fa8880bcf336e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function searchError(msg) {
+	$("#error-msg").html(msg)
+}
+
+$(document).ready(() => {
+	const db = firebase.firestore()
+
+	$("#go-button").click((e) => {
+		const sesh = $("#session-id-input").val()
+		if (sesh.length == 0) {
+			searchError("Please enter sesh ID");
+			return;
+		}
+
+		db.collection("sessions").doc(sesh).get().then((session) => {
+			const users = session.data();
+			if (users == undefined) {
+				searchError("Could not find sesh :(");
+			}
+		}).catch(err => console.log(err));
+	})
+});
